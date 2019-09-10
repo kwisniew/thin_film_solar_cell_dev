8624a335c6a50230379d8dfe8256b9bdec7d9b91
kon <kon.wu888@gmail.com>
2019-09-10 16:51:50 +0200

    better initial conditions
    1) Full depletion approximation is now used to give an initial
    condition.
    2) Initial conditions are now separate from doping profile
    3) as well as electron and hole densities for LDG boundary

--- include/InitialConditions.hpp
+++ include/InitialConditions.hpp
@@ -5,13 +5,13 @@
 
 using namespace dealii;
 
-/** \brief Initial/Boundary conditions for electrons, \f$ \rho_{n}^{e} \f$.*/
+/** \brief Set doping profile for donors (e.g. 0 on p-type side and Nd for n-type side.*/
 template<int dim>
-class Electrons_Equilibrium : public Function<dim>
+class Doping_profile_donors : public Function<dim>
 {
 	public:
 		/** \brief Default constructor. */
-		Electrons_Equilibrium() : Function<dim>(dim+1)
+		Doping_profile_donors() : Function<dim>(dim+1)
 		{};
 
 		void set_values(const double & n_type_donor_doping, const double & p_type_donor_doping, const double & p_type_width);
@@ -30,13 +30,13 @@ class Electrons_Equilibrium : public Function<dim>
 		double scaled_p_type_width;
 };
 
-	/** \brief Initial/Boundary conditions for holes, \f$ \rho_{p}^{e} \f$.*/
+	/** \brief Set doping profile for acceptors (e.g. Na on p-type side and 0 for n-type side.*/
 	template<int dim>
-	class Holes_Equilibrium : public Function<dim>
+	class Doping_profile_acceptors : public Function<dim>
 	{
 		public:
 			/** \brief Default constructor. */
-			Holes_Equilibrium() : Function<dim>(dim+1)
+			Doping_profile_acceptors() : Function<dim>(dim+1)
 			{}
 
 			void set_values(const double & n_type_acceptor_doping, const double & p_type_acceptor_doping, const double & p_type_width);
@@ -51,35 +51,117 @@ class Electrons_Equilibrium : public Function<dim>
 			double scaled_p_type_width;
 	};
 
-	/** \brief Initial/Boundary conditions for reductants, \f$ \rho_{r}^{\infty} \f$.*/
+	/** \brief Initial conditions for electrons in full depletion approximation.
+	 * That is: zero carrier density near to pn interface.
+	 *  \f$ \rho_{r}^{\infty} \f$.*/
 	template<int dim>
-	class Reductants_Equilibrium : public Function<dim>
+	class Initial_condition_electrons : public Function<dim>
 	{
 		public:
 			/** \brief Default constructor. */
-			Reductants_Equilibrium() : Function<dim>(dim+1)
+			Initial_condition_electrons() : Function<dim>(dim+1)
 			{}
+
+			void set_values(const double & n_type_donor_density,
+							const double & p_type_donor_density,
+							const double & p_type_width,
+							const double & n_type_depletion_width,
+							const double & p_type_depletion_width);
+
 
 			/** \brief Returns value of \f$\rho_{r}^{\infty}\f$ at point p.*/
 			virtual double value(const Point<dim> & p,
 					     const unsigned int component=0) const;
+
+		private:
+			double scaled_n_type_donor_density;
+			double scaled_p_type_donor_density;
+			double scaled_p_type_width;
+			double scaled_n_type_depletion_width;
+			double scaled_p_type_depletion_width;
 	};
 
-	
 
-	/** \brief Initial/Boundary conditions for oxidants, \f$ \rho_{o}^{\infty} \f$.*/
+
+	/** \brief Initial conditions for electrons in full depletion approximation.
+	 * That is: zero carrier density near to pn interface.
+	 *  \f$ \rho_{o}^{\infty} \f$.*/
 	template<int dim>
-	class Oxidants_Equilibrium : public Function<dim>
+	class Initial_condition_holes : public Function<dim>
 	{
 		public:
 			/** \brief Default constructor. */
-			Oxidants_Equilibrium() : Function<dim>(dim+1)
+			Initial_condition_holes() : Function<dim>(dim+1)
 			{}
 
-			/** \brief Returns value of \f$\rho_{o}^{\infty}\f$ at point p.*/
+			void set_values(const double & n_type_acceptor_density,
+							const double & p_type_acceptor_density,
+							const double & p_type_width,
+							const double & n_type_depletion_width,
+							const double & p_type_depletion_width);
+
+
+			/** \brief Returns value of \f$\rho_{r}^{\infty}\f$ at point p.*/
 			virtual double value(const Point<dim> & p,
 					     const unsigned int component=0) const;
+
+		private:
+			double scaled_n_type_acceptor_density;
+			double scaled_p_type_acceptor_density;
+			double scaled_p_type_width;
+			double scaled_n_type_depletion_width;
+			double scaled_p_type_depletion_width;
 	};
 
+	template<int dim>
+	class LDG_Dirichlet_electron_density_bc : public Function<dim>
+	{
+		public:
+			/** \brief Default constructor. */
+			LDG_Dirichlet_electron_density_bc() : Function<dim>(dim+1)
+			{}
+
+			void set_values(const double & n_type_donor_density,
+							const double & p_type_donor_density,
+							const double & p_type_width,
+							const double & n_type_width);
+
+
+			/** \brief Returns value of \f$\rho_{r}^{\infty}\f$ at point p.*/
+			virtual double value(const Point<dim> & p,
+					     const unsigned int component=0) const;
+
+		private:
+			double scaled_n_type_donor_density;
+			double scaled_p_type_donor_density;
+			double scaled_p_type_width;
+			double scaled_n_type_width;
+	};
+
+
+	template<int dim>
+	class LDG_Dirichlet_hole_density_bc : public Function<dim>
+	{
+		public:
+			/** \brief Default constructor. */
+			LDG_Dirichlet_hole_density_bc() : Function<dim>(dim+1)
+			{}
+
+			void set_values(const double & n_type_acceptor_density,
+							const double & p_type_acceptor_density,
+							const double & p_type_width,
+							const double & n_type_width);
+
+
+			/** \brief Returns value of \f$\rho_{r}^{\infty}\f$ at point p.*/
+			virtual double value(const Point<dim> & p,
+					     const unsigned int component=0) const;
+
+		private:
+			double scaled_n_type_acceptor_density;
+			double scaled_p_type_acceptor_density;
+			double scaled_p_type_width;
+			double scaled_n_type_width;
+	};
 
 #endif
--- source/InitialConditions.cpp
+++ source/InitialConditions.cpp
@@ -4,7 +4,7 @@
 
 	template <int dim>
 	double
-	Electrons_Equilibrium<dim>::
+	Doping_profile_donors<dim>::
 	value(const Point<dim> &p,
 		  const unsigned int component) const
 	{
@@ -25,7 +25,7 @@
 
 	template <int dim>
 	void
-	Electrons_Equilibrium<dim>::
+	Doping_profile_donors<dim>::
 	set_values(const double & n_type_donor_doping, const double & p_type_donor_doping, const double & p_type_width)
 	{
 		scaled_n_type_donor_doping = n_type_donor_doping;
@@ -38,7 +38,7 @@
 
 	template <int dim>
 	double
-	Holes_Equilibrium<dim>::
+	Doping_profile_acceptors<dim>::
 	value(const Point<dim> &p,
 		  const unsigned int component) const
 	{
@@ -60,7 +60,7 @@
 
 	template <int dim>
 	void
-	Holes_Equilibrium<dim>::
+	Doping_profile_acceptors<dim>::
 	set_values(const double & n_type_acceptor_doping, const double & p_type_acceptor_doping, const double & p_type_width)
 	{
 		scaled_n_type_acceptor_doping = n_type_acceptor_doping;
@@ -71,7 +71,7 @@
 
 	template <int dim>
 	double
-	Reductants_Equilibrium<dim>::
+	Initial_condition_electrons<dim>::
 	value(const Point<dim> &p,
 		  const unsigned int component) const
 	{
@@ -83,13 +83,39 @@
 		}
 		else //(component == dim)
 		{
-				return 30; 
+			if(p[0] < (scaled_p_type_width - scaled_p_type_depletion_width) )
+				return scaled_p_type_donor_density;
+			else
+			{
+				if(p[0] < (scaled_p_type_width + scaled_n_type_depletion_width) )
+					return 0;
+				else
+					return scaled_n_type_donor_density;
+			}
 		}
 	}
 
 	template <int dim>
+	void
+	Initial_condition_electrons<dim>::
+	set_values( const double & n_type_donor_density,
+				const double & p_type_donor_density,
+				const double & p_type_width,
+				const double & n_type_depletion_width,
+				const double & p_type_depletion_width)
+	{
+		scaled_n_type_donor_density   = n_type_donor_density;
+		scaled_p_type_donor_density   = p_type_donor_density;
+		scaled_p_type_width  		  = p_type_width;
+		scaled_n_type_depletion_width = n_type_depletion_width;
+		scaled_p_type_depletion_width = p_type_depletion_width;
+
+	}
+
+
+	template <int dim>
 	double
-	Oxidants_Equilibrium<dim>::
+	Initial_condition_holes<dim>::
 	value(const Point<dim> &p,
 		  const unsigned int component) const
 	{
@@ -101,7 +127,121 @@
 		}
 		else //(component == dim)
 		{
-				return 29; 
+			if(p[0] < (scaled_p_type_width - scaled_p_type_depletion_width) )
+				return scaled_p_type_acceptor_density;
+			else
+			{
+				if(p[0] < (scaled_p_type_width + scaled_n_type_depletion_width) )
+					return 0;
+				else
+					return scaled_n_type_acceptor_density;
+			}
 		}
 	}
 
+	template <int dim>
+	void
+	Initial_condition_holes<dim>::
+	set_values( const double & n_type_acceptor_density,
+				const double & p_type_acceptor_density,
+				const double & p_type_width,
+				const double & n_type_depletion_width,
+				const double & p_type_depletion_width)
+	{
+		scaled_n_type_acceptor_density = n_type_acceptor_density;
+		scaled_p_type_acceptor_density = p_type_acceptor_density;
+		scaled_p_type_width  		   = p_type_width;
+		scaled_n_type_depletion_width  = n_type_depletion_width;
+		scaled_p_type_depletion_width  = p_type_depletion_width;
+
+	}
+
+
+	template <int dim>
+	double
+	LDG_Dirichlet_electron_density_bc<dim>::
+	value(const Point<dim> &p,
+		  const unsigned int component) const
+	{
+			// dim+1 components
+		if(component < dim)
+		{
+			// set the components of the current initially to zero
+			return ZeroFunction<dim>(dim+1).value(p, component);
+		}
+		else //(component == dim)
+		{
+			if(p[0] < 1e-6 )
+				return scaled_p_type_donor_density;
+			else
+			{
+				if(p[0] > (scaled_p_type_width + scaled_n_type_width - 1e-6) )
+					return scaled_n_type_donor_density;
+				else
+				{
+					//std::cout<<"This situation is not implemented!" <<std::endl;
+					return 0;
+				}
+
+			}
+		}
+	}
+
+	template <int dim>
+	void
+	LDG_Dirichlet_electron_density_bc<dim>::
+	set_values( const double & n_type_donor_density,
+				const double & p_type_donor_density,
+				const double & p_type_width,
+				const double & n_type_width)
+	{
+		scaled_n_type_donor_density = n_type_donor_density;
+		scaled_p_type_donor_density = p_type_donor_density;
+		scaled_p_type_width  		= p_type_width;
+		scaled_n_type_width         = n_type_width;
+
+	}
+
+	template <int dim>
+	double
+	LDG_Dirichlet_hole_density_bc<dim>::
+	value(const Point<dim> &p,
+		  const unsigned int component) const
+	{
+			// dim+1 components
+		if(component < dim)
+		{
+			// set the components of the current initially to zero
+			return ZeroFunction<dim>(dim+1).value(p, component);
+		}
+		else //(component == dim)
+		{
+			if(p[0] < 1e-6 )
+				return scaled_p_type_acceptor_density;
+			else
+			{
+				if(p[0] > (scaled_p_type_width + scaled_n_type_width - 1e-6) )
+					return scaled_n_type_acceptor_density;
+				else
+				{
+					//std::cout<<"This situation is not implemented!" <<std::endl;
+					return 0;
+				}
+
+			}
+		}
+	}
+
+	template <int dim>
+	void
+	LDG_Dirichlet_hole_density_bc<dim>::
+	set_values( const double & n_type_acceptor_density,
+				const double & p_type_acceptor_density,
+				const double & p_type_width,
+				const double & n_type_width)
+	{
+		scaled_n_type_acceptor_density = n_type_acceptor_density;
+		scaled_p_type_acceptor_density = p_type_acceptor_density;
+		scaled_p_type_width  		= p_type_width;
+		scaled_n_type_width         = n_type_width;
+	}
